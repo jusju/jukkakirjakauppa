07f219bfcf4f91c456edf20f058011ac45e00b55
jusju <jukka.juslin@haaga-helia.fi>
2022-01-27 13:05:33 +0200

    first commat

---
+++ src/main/java/com/example/jukkakirjakauppa/JukkakirjakauppaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jukkakirjakauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukkakirjakauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JukkakirjakauppaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/jukkakirjakauppa/domain/Book.java
@@ -0,0 +1,9 @@
+package com.example.jukkakirjakauppa.domain;
+
+public class Book {
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private double price;
+}
